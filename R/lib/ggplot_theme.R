79e90988928ce739924f3d5355b679ad8f39abe8
audy <harekrishna@gmail.com>
2014-03-10 14:10:50 -0400

    added theme

---
+++ R/lib/ggplot_theme.R
@@ -0,0 +1,19 @@
+library(ggplot2)
+
+
+ggplot.theme <- theme(panel.background = element_blank(),
+                      strip.background = element_blank(),
+                      panel.grid.major = element_line(),
+                      panel.grid.minor = element_blank(),
+                      axis.line = element_line(),
+                      legend.position='bottom')
+
+ggplot.pal.scale <- scale_fill_manual(values=c("#FFAB00", # cases
+                                             "#999999") # controls
+                                        )
+
+
+ggplot.pal.sig <- scale_colour_manual(values=c("#218457", # 0.05
+                                  "#00B060", # 0.01
+                                   "#FFFFFF") # insignificant
+)
