791ed234fdbe469fddf9d6aedb281adae73122c4
audy <harekrishna@gmail.com>
2014-03-14 14:55:32 -0400

    added small function for plotting timeserieses with geom_smooth

---
+++ R/lib/plot-timeseries.R
@@ -0,0 +1,7 @@
+plot_timeseries <- function(g, x=NULL, y=NULL, color=NULL) {
+    mdf <- psmelt(g)
+
+  p <- ggplot(mdf, aes_string(y='Abundance', x=x, color=color)) + geom_smooth()
+
+    return(p)
+}
