db76e184e41156855deca9bec8d5e9ef9964e8cc
audy <harekrishna@gmail.com>
2014-03-14 14:16:00 -0400

    added arguments to write-rdata

--- R/lib/load_dipp.R
+++ R/lib/load_dipp.R
@@ -1,10 +1,6 @@
 library(phyloseq)
 
-OTU.TABLE <- '../data/dipp.gg135.97.50r10s.csv'
-SAM.DATA <- '../data/dipp.sample_data.csv'
-TAX.TABLE <- '../data/dipp.modified_taxonomies.csv'
-
-'load.dipp' <- function(otu.table=OTU.TABLE, sam.data=SAM.DATA, tax.table=TAX.TABLE) {
+'load.dipp' <- function(otu.table=NONE, sam.data=NONE, tax.table=NONE, tree=NONE) {
   otus <- otuTable(read.csv(otu.table, header=T, row.names=1, check.names=F), taxa_are_rows=F)
   meta <- samData(read.csv(sam.data, header=T, row.names=1))
   taxa <- taxTab(as.matrix(read.csv(tax.table, header=T, row.names=1)))
