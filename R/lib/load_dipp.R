ae2f419acdbc80310e17568027404acd450f5f03
Austin Davis-Richardson <harekrishna@gmail.com>
2014-05-16 10:16:45 -0400

    add Sp. for missing species names

--- R/lib/load_dipp.R
+++ R/lib/load_dipp.R
@@ -5,7 +5,13 @@ library(phyloseq)
 'load.dipp' <- function(otu.table=NULL, sam.data=NULL, tax.table=NULL, tree=NULL) {
   otus <- otuTable(read.csv(otu.table, header=T, row.names=1, check.names=F), taxa_are_rows=F)
   meta <- samData(read.csv(sam.data, header=T, row.names=1))
-  taxa <- taxTab(as.matrix(read.csv(tax.table, header=T, row.names=1)))
+  taxa <- read.csv(tax.table, header=T, row.names=1)
+
+  # combine genus and species names
+  taxa[taxa$Species == '',]$Species <- 'sp.'
+  taxa$Species <- paste(taxa$Genus, taxa$Species)
+
+  taxa <- taxTab(as.matrix(taxa))
 
   if (!is.null(tree)) tree <- read_tree_greengenes(tree)
 
