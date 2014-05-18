8338a2bc474ba728534d40907d1a8e44a6ce5e22
Austin Davis-Richardson <harekrishna@gmail.com>
2014-05-18 17:44:26 -0400

    fixed deprecation warnings

--- R/lib/load_dipp.R
+++ R/lib/load_dipp.R
@@ -3,15 +3,15 @@
 library(phyloseq)
 
 'load.dipp' <- function(otu.table=NULL, sam.data=NULL, tax.table=NULL, tree=NULL) {
-  otus <- otuTable(read.csv(otu.table, header=T, row.names=1, check.names=F), taxa_are_rows=F)
-  meta <- samData(read.csv(sam.data, header=T, row.names=1))
+  otus <- otu_table(read.csv(otu.table, header=T, row.names=1, check.names=F), taxa_are_rows=F)
+  meta <- sample_data(read.csv(sam.data, header=T, row.names=1))
   taxa <- read.csv(tax.table, header=T, row.names=1)
 
   # combine genus and species names
   taxa[taxa$Species == '',]$Species <- 'sp.'
   taxa$Species <- paste(taxa$Genus, taxa$Species)
 
-  taxa <- taxTab(as.matrix(taxa))
+  taxa <- tax_table(as.matrix(taxa))
 
   if (!is.null(tree)) tree <- read_tree_greengenes(tree)
 
