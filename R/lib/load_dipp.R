68fc4d7fee81995fcbcb8f95cac12e55c0b25f22
Austin Davis-Richardson <harekrishna@gmail.com>
2014-05-21 14:55:13 -0400

    dont fix names

--- R/lib/load_dipp.R
+++ R/lib/load_dipp.R
@@ -7,10 +7,6 @@ library(phyloseq)
   meta <- sample_data(read.csv(sam.data, header=T, row.names=1))
   taxa <- read.csv(tax.table, header=T, row.names=1)
 
-  # combine genus and species names
-  taxa[taxa$Species == '',]$Species <- 'sp.'
-  taxa$Species <- paste(taxa$Genus, taxa$Species)
-
   taxa <- tax_table(as.matrix(taxa))
 
   if (!is.null(tree)) tree <- read_tree_greengenes(tree)
